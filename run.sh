8ed3e3521cba68aa1b3048642c632372f7c83315
guillermo.palli <guillermo.palli@vinculomedico.com.ar>
2017-04-11 11:35:58 -0300

    Add check environment variable for debugging tomcat

--- run.sh
+++ run.sh
@@ -7,7 +7,11 @@ rm -f log.out
 
 nohup httpd -DFOREGROUND >log.out 2>&1 &
 
-nohup catalina.sh run >>log.out 2>&1 &
+if [ -z "$JPDA_ADDRESS" ]; then
+  nohup catalina.sh run >>log.out 2>&1 &
+else
+  nohup catalina.sh jpda run >>log.out 2>&1 &
+fi
 
 tail -f log.out
 
